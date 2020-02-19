b7ccf05800b70f556f25a529e48fc16164518f7c
ygolin <hfm293@163.com>
2020-02-19 23:29:03 +0800

    11

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,29 @@
 <template>
   <div id="app">
     <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+      <router-link to="/"><span class="text">Home</span></router-link> |
+      <router-link to="/about"><span class="text">About</span></router-link>
     </div>
     <router-view/>
   </div>
 </template>
 
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-#nav {
-  padding: 30px;
-}
+<script>
+  export default {}
+  // 设置rem 最大50像素，为浏览器宽度十分之一 还有使用scss
+  document.addEventListener('DOMContentLoaded', () => {
+  const html = document.querySelector('hmtl')
+  let fontSize = window.innerWidth / 10
+  fontSize = fontSize > 50 ? 50 : fontSize
+  html.style.fontSize = fontSize + 'px'
+  })
 
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
+</script>
 
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
+<style scoped lang="scss">
+  #app {
+    width: 100%;
+    height: 100%;
+    overflow: hidden;
+  }
 </style>
