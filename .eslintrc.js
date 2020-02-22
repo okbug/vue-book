000472f500a3560e5c4317f9256275587df365e9
cxwht <cxwht@qq.com>
2020-02-22 18:17:08 +0800

    update

--- .eslintrc.js
+++ .eslintrc.js
@@ -10,8 +10,7 @@ module.exports = {
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-    'indent': 'off',
-    'no-trailing--spaces' : 'off'
+    'indent': 'off'
   },
   parserOptions: {
     parser: 'babel-eslint'
